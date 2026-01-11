4e4dd65af9b2f71765cc85d3d3d9c4c0c7d8fe48
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-01-12 02:10:52 +0700

    Adding Admin Dashboard

--- deploy.sh
+++ deploy.sh
@@ -42,6 +42,6 @@ echo "--- Finalizing Permissions ---"
 sudo chown www-data:www-data "$DB_FILE"
 sudo chmod 664 "$DB_FILE"
 sudo chown -R www-data:www-data $PROJECT_DIR
-sudo chown -R $USER:$USER "$PROJECT_DIR/deploy.sh"
+sudo chown $USER:$USER "$PROJECT_DIR/deploy.sh"
 sudo chmod +x "$PROJECT_DIR/deploy.sh"
 echo "=== DEPLOYMENT SELESAI & SUKSES! ==="
