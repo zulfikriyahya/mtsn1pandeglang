beaff60174338acc0f0711091b293fae15d6b212
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-01-11 22:22:21 +0700

    Update

--- src/lib/utils/readingTime.ts
+++ src/lib/utils/readingTime.ts
@@ -25,15 +25,10 @@ const readingTime = (content: string): string => {
   }
 
   const minutes = Math.ceil(((words - imageAdjust) / WPS + imageSecs) / 60);
-
   if (minutes < 10) {
-    if (minutes < 2) {
-      return "0" + minutes + ` Min read`;
-    } else {
-      return "0" + minutes + ` Mins read`;
-    }
+    return "0" + minutes + ` Menit baca`;
   } else {
-    return minutes + ` Mins read`;
+    return minutes + ` Menit baca`;
   }
 };
 
