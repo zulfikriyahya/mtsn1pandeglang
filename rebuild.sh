21ad53085a4265e91d960ee7a4c646c3b3e420ec
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-01-13 05:08:15 +0700

    Update

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,4 @@
 
 # Panggil script deploy.sh yang canggih (Smart Sync)
 # Outputnya kita lempar ke log file
-sudo -u www-data ./deploy.sh >> /var/log/web_build.log 2>&1
+./deploy.sh >> /var/log/web_build.log 2>&1
