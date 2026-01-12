a433316e4275844520e8954403deb02c261b5fb2
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-01-13 04:21:51 +0700

    Permission

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,4 @@
 
 # Panggil script deploy.sh yang canggih (Smart Sync)
 # Outputnya kita lempar ke log file
-sudo -u zulfikriyahya /bin/bash /var/www/mtsn1pandeglang.sch.id/deploy.sh >> /var/log/web_build.log 2>&1
+/bin/bash /var/www/mtsn1pandeglang.sch.id/deploy.sh >> /var/log/web_build.log 2>&1
